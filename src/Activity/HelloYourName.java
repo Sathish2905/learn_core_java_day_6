7a444224f3aea556a86f6371624dfd71a1a08b57
Sathish2905 <sathishkumart@logiwareinc.in>
2018-05-15 17:51:26 +0530

    first commit

---
+++ src/Activity/HelloYourName.java
@@ -0,0 +1,35 @@
+/* Challenge Activity 3 - Hello your name, object style
+
+In this challenge activity, You'll write the familiar "hello your name" program
+only using an object of the custom class YourName. 
+
+The main program should:
+1) ask for your first name
+2) ask for your last name
+3) create a YourName object using first and last name
+4) print "Hello (your full name)"
+5) print "Or should I say: (your name last, first)
+
+NOTE: before you complete this program you must first implement YourName.java!!!
+
+EXAMPLE:
+
+Enter your FIRST name: Sathish 
+Enter your LAST name: kumar
+Hello, Sathish kumar!
+Or should I say: kumar, Sathish
+
+ */
+package Activity;
+
+/**
+ *
+ * @author Sathishkumar T
+ */
+public class HelloYourName {
+
+    public static void main(String[] args) {
+        // TODO: Code your application logic here.
+
+    }
+}
